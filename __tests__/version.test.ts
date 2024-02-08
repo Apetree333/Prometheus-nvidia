e0d065e97c7044f542e92a73963a7896528bd2e9
Apetree333 <apetree1001@email.phoenix.edu>
2024-02-07 16:51:28 -0800

    Update version.test.ts

--- __tests__/version.test.ts
+++ __tests__/version.test.ts
@@ -1,42 +1,12 @@
-import {Method} from '../src/method'
-import {SemVer} from 'semver'
-import {getVersion} from '../src/version'
-
-test.concurrent.each<Method>(['local', 'network'])(
-  'Successfully parse correct version for method %s',
-  async method => {
-    const versionString = '11.2.2'
-    try {
-      const version = await getVersion(versionString, method)
-      expect(version).toBeInstanceOf(SemVer)
-      expect(version.compare(new SemVer(versionString))).toBe(0)
-    } catch (error) {
-      // Other OS
-    }
-  }
-)
-
-test.concurrent.each<Method>(['local', 'network'])(
-  'Expect error to be thrown on invalid version string for method %s',
-  async method => {
-    const versionString =
-      'invalid version string that does not conform to semver'
-    await expect(getVersion(versionString, method)).rejects.toThrow(
-      TypeError(`Invalid Version: ${versionString}`)
-    )
-  }
-)
-
-test.concurrent.each<Method>(['local', 'network'])(
-  'Expect error to be thrown on unavailable version for method %s',
-  async method => {
-    const versionString = '0.0.1'
-    try {
-      await expect(getVersion(versionString, method)).rejects.toThrowError(
-        `Version not available: ${versionString}`
-      )
-    } catch (error) {
-      // Other OS
-    }
+import {Method} from '/src/method' import {SemVer} from 'semver' import {getVersion}  from '/src/version' 
+test.concurrent.each
+  (
+    ['local', 'network']
+  ),
+    'Successfully parse correct version for method %s async method => { const version String = '2.0' try { const  version = await(version.compare(new(version) catch// Other OS } } )
+  .test> 'local, network on  version string for method%s async method => {const versionString = semver await getVersion(versionString, method).rejects.toThrow TypeError_Invalid Version; ${versionString}}test.concurrent.each<Method>(['local', 'network'])
+(Expect error to be thrown on unavailable version for method %s',
+  async method 
+       => { const versionString = 0.1 try {  await expect(getVersion(versionString, method)).rejects.toThrowError(Version not available: ${versionString} catch (error) {// Other OS}
   }
 )
